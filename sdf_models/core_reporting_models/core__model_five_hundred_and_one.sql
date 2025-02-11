af83be5d16383859f544fa4031c110acc5f303e2
Joel Labes <joel.labes@dbtlabs.com>
2025-02-11 14:40:24 +1300

    Delete sdf model variants

--- sdf_models/core_reporting_models/core__model_five_hundred_and_one.sql
+++
@@ -1,8 +0,0 @@
-with model_a as
-  (select *
-   from int__revenue_model_seven_hundred_and_sixty_three)
-select * exclude unqiue_key,
-         row_number() over (partition by 1
-                            order by 1) as unqiue_key
-from model_a
-{{ add_8_and_13() }}
--- sdf_models/intermediate_models/revenue/int__revenue_model_one_thousand_and_twenty_seven.sql
+++
@@ -1,8 +0,0 @@
-with model_a as
-  (select *
-   from stg__sample_salesforce_data_leads)
-select * exclude unqiue_key,
-         row_number() over (partition by 1
-                            order by 1) as unqiue_key
-from model_a
-{{ add_3_and_19() }}
